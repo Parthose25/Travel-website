e108ccc0935f609a0b20506e57f522b6ab1e30a1
Parthose25 <84158326+Parthose25@users.noreply.github.com>
2021-05-18 03:45:42 +0530

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// side nav-bar collapsable
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+  
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+  $('.datepicker').datepicker({
+    disableWeekends: true,
+    yearRange: 1
+  })
